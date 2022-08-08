293d0bcc95b38278f0aec6a4cc99e4efe180dd0f
Thomas Weinschenk <thomas.weinschenk@endress.com>
2022-08-08 13:23:07 +0200

    Initial commit

---
+++ packages/oi4-oec-service-demo/src/application/ServiceDemoClientCallbacksHelper.ts
@@ -0,0 +1,9 @@
+import {ClientCallbacksHelper} from '@oi4/oi4-oec-service-node/dist/Utilities/Helpers/ClientCallbacksHelper';
+
+export class ServiceDemoClientCallbacksHelper extends ClientCallbacksHelper {
+
+    constructor() {
+        super();
+    }
+
+}
