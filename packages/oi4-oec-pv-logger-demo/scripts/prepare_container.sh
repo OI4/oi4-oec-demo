f5b7be24bc1508f722fbbd5704070556a4153fb3
Thomas Weinschenk <thomas.weinschenk@endress.com>
2024-06-20 09:54:05 +0200

    Build check added

--- packages/oi4-oec-pv-logger-demo/scripts/prepare_container.sh
+++ packages/oi4-oec-pv-logger-demo/scripts/prepare_container.sh
@@ -11,4 +11,13 @@ cp -r ./dist/ ./build/container/dist
 
 cd ./build/container || exit
 yarn install --production
+
+if [[ -f ./dist/app.js ]]
+then
+  echo "app.js found"
+else
+  echo "app.js not found"
+  exit 1
+fi
+
 cd ../..
