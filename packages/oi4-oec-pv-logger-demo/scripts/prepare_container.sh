90e25b83c323d05a321b724c2c5add610bc3acf3
Thomas Weinschenk <thomas.weinschenk@endress.com>
2024-06-20 10:10:52 +0200

    Fixed build issues

--- packages/oi4-oec-pv-logger-demo/scripts/prepare_container.sh
+++ packages/oi4-oec-pv-logger-demo/scripts/prepare_container.sh
@@ -3,30 +3,25 @@ rm -rf ./build
 
 mkdir -p ./build/container
 mkdir -p ./build/container/scripts
-#mkdir -p ./build/container/dist
 
 cp ./package.json ./build/container/package.json
 cp ./scripts/entrypoint.sh ./build/container/scripts/entrypoint.sh
 
-if [[ -f ./dist/app.js ]]
+if [[ ! -f ./dist/app.js ]]
 then
-  echo "app.js found in build dist"
-else
-  echo "app.js not found in build dist"
+  echo "build step not executed or failed"
   exit 1
 fi
 
 cp -r ./dist/ ./build/container/dist
 
-cd ./build/container || exit
-yarn install --production
-
-if [[ -f ./dist/app.js ]]
+if [[ ! -f ./dist/app.js ]]
 then
-  echo "app.js found"
-else
-  echo "app.js not found"
+  echo "Copying dist failed"
   exit 1
 fi
+
+cd ./build/container || exit
+yarn install --production
 
 cd ../..
--- packages/oi4-oec-service-demo/scripts/prepare_container.sh
+++ packages/oi4-oec-service-demo/scripts/prepare_container.sh
@@ -3,11 +3,23 @@ rm -rf ./build
 
 mkdir -p ./build/container
 mkdir -p ./build/container/scripts
-mkdir -p ./build/container/dist
 
 cp ./package.json ./build/container/package.json
 cp ./scripts/entrypoint.sh ./build/container/scripts/entrypoint.sh
+
+if [[ ! -f ./dist/app.js ]]
+then
+  echo "build step not executed or failed"
+  exit 1
+fi
+
 cp -r ./dist/ ./build/container/dist
+
+if [[ ! -f ./dist/app.js ]]
+then
+  echo "Copying dist failed"
+  exit 1
+fi
 
 cd ./build/container || exit
 yarn install --production
