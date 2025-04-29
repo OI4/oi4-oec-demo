d686c842387538a6e09c57a5f7ed33f65f216ae7
Thomas <ger.onimo@gmx.de>
2025-04-29 09:03:57 +0200

    fixing build issues

--- packages/oi4-oec-pv-logger-demo/scripts/prepare_container.sh
+++ packages/oi4-oec-pv-logger-demo/scripts/prepare_container.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 rm -rf ./build
 rm -rf ./node_modules
+rm -rf .yarncache
+
 yarn cache clean
 
 mkdir -p ./build/container
 mkdir -p ./build/container/scripts
+mkdir .yarncache
 
 cp ./package.json ./build/container/package.json
 cp ./scripts/entrypoint.sh ./build/container/scripts/entrypoint.sh
@@ -24,6 +27,7 @@ then
 fi
 
 cd ./build/container || exit
-yarn install --production
+yarn install --production --cache-folder ./.yarncache
+ rm -rf .yarncache
 
 cd ../..
